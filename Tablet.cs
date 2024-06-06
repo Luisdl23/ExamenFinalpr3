ab0966b7c0d9b0480afd0e94ef482186475a1a68
user <quique23@umes.edu.gt>
2024-06-06 08:11:47 -0600

    Ingreso de clases

---
+++ Tablet.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace ExamenFinalpr3
+{
+    public class Tablet:Dispositivos
+    {
+        int Tamaniopantalla { get; set; }
+        string SoporteparaStylus { get; set; }
+
+        public Tablet()
+        {
+            Tamaniopantalla = 0;
+            SoporteparaStylus = string.Empty;
+        }
+    }
+}
